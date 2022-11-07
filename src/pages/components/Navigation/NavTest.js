6bcb63ac82ca7b4ecee4c014c6db48d7ed74ed78
flaviobartolo <flavio.bartolo2@gmail.com>
2022-11-07 23:06:39 +0000

    lang and theme icons

--- src/pages/components/Navigation/NavTest.js
+++ src/pages/components/Navigation/NavTest.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import { Link } from 'react-router-dom'
 import { RiMoonClearLine } from 'react-icons/ri'
+import { BsSun } from 'react-icons/bs'
 
 import './NavTest.css'
 
 const NavTest = () => {
+
+  const [dayIcon, setDayIcon] = useState(true)
+  const [langPT, setLangPT] = useState(true)
+
+  const toggleDayNight = () => {
+    setDayIcon(!dayIcon)
+  }
+
+  const toggleLang = () => {
+    setLangPT(!langPT)
+  }
+
   return <>
   <nav className='main-navigation-desktop'>
     <div className='main-navigation-desktop__left-container'>
@@ -24,9 +38,18 @@ const NavTest = () => {
       </li>
     </ul>
     <div className='main-navigation-desktop__right-container'>
-      <h4 className='main-navigation-desktop__theme'>
-        <Link to='/'><RiMoonClearLine /></Link>
-      </h4>
+        <ul>
+          <li>
+            <Link onClick={toggleLang}> 
+              {langPT ?  'PT' :  'EN' }
+            </Link>
+          </li>
+          <li>
+            <Link onClick={toggleDayNight}>
+              {dayIcon ?  <RiMoonClearLine /> :  <BsSun />}
+            </Link>
+          </li>
+        </ul>
     </div>
   </nav>
   </>
